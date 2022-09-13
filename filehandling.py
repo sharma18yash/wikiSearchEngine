8165bbf619296518e8d8edea1211914e97c1d54d
Yash Sharma <37216644+sharma18yash@users.noreply.github.com>
2022-09-13 23:58:21 +0530

    secondary index created

--- filehandling.py
+++ filehandling.py
@@ -3,6 +3,8 @@ from curses.ascii import isalpha
 import json
 from xml.dom.minidom import Element
 import heap
+from collections import OrderedDict
+
 
 from collections import Counter
 
@@ -32,7 +34,7 @@ class file_handling:
 
         hp.minheap()
 
-        final_dict = {}
+        final_dict = OrderedDict()
         index_count = 0
         unique_tokens = 0
         prev = '0'
@@ -43,29 +45,41 @@ class file_handling:
                 value = ""
             value = value.strip("\n")
 
-            if len(key) > 0:
-                if key[0] != prev:
-                    # print(key[0], prev)
-                    prev = key[0]
-                    if ( ord(key[0]) >= 97 and ord(key[0]) <= 122) or ( ord(key[0]) >= 48 and ord(key[0]) <= 57):
-                        # print("printing key[0]: ", key[0])
-                        with open('tmp/final_index{}.txt'.format(index_count), 'a') as f: 
-                            for key, value in final_dict.items():
-                                # if(key[0] == 'z'):
-                                    # print(key, index_count) 
-                                f.write('%s~%s\n'% (key, value))
-                        index_count+=1
-                        unique_tokens += len(final_dict)
-                        final_dict = {}
-                    else:
-                        # print("misc :",  key[0])
-                        with open('tmp/misc.txt', 'a') as f: 
-                            for key, value in final_dict.items(): 
-                                f.write('%s~%s\n'% (key, value))
-                        unique_tokens += len(final_dict)
-                        final_dict = {}
+            # if len(key) > 0:
+            #     if key[0] != prev:
+            #         # print(key[0], prev)
+            #         prev = key[0]
+            #         if ( ord(key[0]) >= 97 and ord(key[0]) <= 122) or ( ord(key[0]) >= 48 and ord(key[0]) <= 57):
+            #             # print("printing key[0]: ", key[0])
+            #             with open('tmp/final_index{}.txt'.format(index_count), 'a') as f: 
+            #                 for key, value in final_dict.items():
+            #                     # if(key[0] == 'z'):
+            #                         # print(key, index_count) 
+            #                     f.write('%s~%s\n'% (key, value))
+            #             index_count+=1
+            #             unique_tokens += len(final_dict)
+            #             final_dict = {}
+            #         else:
+            #             # print("misc :",  key[0])
+            #             with open('tmp/misc.txt', 'a') as f: 
+            #                 for key, value in final_dict.items(): 
+            #                     f.write('%s~%s\n'% (key, value))
+            #             unique_tokens += len(final_dict)
+            #             final_dict = {}
             
+            if len(final_dict)%100000==0 and len(final_dict) != 0:
 
+                with open('tmp/secondary_index.txt', 'a') as f:  
+                    f.write('%s~%s\n'% (next(iter(final_dict)), next(reversed(final_dict))))
+
+                with open('tmp/final_index{}.txt'.format(index_count), 'a') as f: 
+                        for key, value in final_dict.items():
+                            # if(key[0] == 'z'):
+                                # print(key, index_count) 
+                            f.write('%s~%s\n'% (key, value))
+                index_count+=1
+                unique_tokens += len(final_dict)
+                final_dict = OrderedDict()
 
             if key in final_dict.keys():
                 final_dict[key] += value
@@ -80,14 +94,15 @@ class file_handling:
                 else:
                     key = data[0]
                     value = data[1]
+                    # if len(key) <= 10 :
                     hp.insert([key, fp, value])
 
-        with open('tmp/misc.txt', 'a') as f: 
-            # print("printing prev: ", prev)
-            for key, value in final_dict.items(): 
-                f.write('%s~%s\n'% (key, value))
-            unique_tokens+= len(final_dict)
-            final_dict = {}
+        # with open('tmp/misc.txt', 'a') as f: 
+        #     # print("printing prev: ", prev)
+        #     for key, value in final_dict.items(): 
+        #         f.write('%s~%s\n'% (key, value))
+        #     unique_tokens+= len(final_dict)
+        #     final_dict = {}
 
 
         print("Unique tokens count: ", unique_tokens)
--- merge.py
+++ merge.py
@@ -1,13 +1,16 @@
 import filehandling
 import os
 import sys
-
+import time
 index_count = sys.argv[1]
 index_count = int(index_count)
+t1 = time.time()
 print("MERGING FILES")
 fh = filehandling.file_handling(index_count)
 secondary_index_files = fh.merge()
+t2 = time.time()
 print("SECONDARY FILES CREATED: ", secondary_index_files )
+print("TIME TAKEN TO COMPLETE SECONDARY INDEXING: ", t2-t1)
 
 print("REMVOVING TEMPORARY FILES")
 
